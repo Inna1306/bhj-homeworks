90e6e433e6a07bcf2b186c3fc9af6750aa8f8014
Inna1306 <inna742@gmail.com>
2025-07-27 10:57:54 +0300

    Выполнено задание

---
+++ js-features/countdown/task.js
@@ -0,0 +1,10 @@
+const countTime = function () {
+    const output = document.getElementById("timer");
+    output.textContent -= 1;
+    if (output.textContent <= 0) {
+        alert('Вы победили в конкурсе!');
+        clearInterval(inervalID);
+    }
+
+};
+let inervalID = setInterval(countTime, 1000);
